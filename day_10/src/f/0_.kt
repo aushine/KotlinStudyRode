0c78bc236cf3ff34b071dad713019e053f5052d4
aushine <1615296610@qq.com>
2019-04-05 22:23:34 +0800

    day_10

--- day_10/src/f/0_.kt
+++ day_10/src/f/0_.kt
@@ -1,7 +1,69 @@
 package f
 
+import kotlin.reflect.KProperty
+
+class Delegate{
+    //用于保存属性值的成员变量
+    var name:String = ""
+    operator  fun getValue(thisRef:Any?,property:KProperty<*>):String{
+        //获取thisRef指定的类名
+        val className = thisRef.toString().substringBefore("@")
+        println("${className}.get已被调用")
+        return name
+    }
+    operator fun setValue(thisRef: Any?,property:KProperty<*>,value:String)
+    {
+        val className = thisRef.toString().substringBefore('@')
+        println("${className}.set已经被调用")
+    }
+
+    }
+class Myclass3{
+    //将name 属性委托给Delegate类
+    var name:String by Delegate()
+}
+class Mycalss4{
+    var name:String by Delegate()
+}
+class MyClass1{
+    var name:String = ""
+        get():String{
+            println("Myclass1.get 已经被调用")
+            return field
+        }
+    set(value:String){
+        println("Myclass1.set 已经被调用")
+        field = value
+    }
+}
+class MyClass2{
+    var name :String = ""
+    get():String{
+        println("MyClass.get已经被调用")
+        return field
+    }
+    set (value:String){
+        println("MyClass.set已经被调用")
+        field = value
+    }
+}
+interface Base{
+    fun print()
+}
+open class BaseImpl(val x:Int):Base{
+    override fun print(){println("$x X_man!")}
+}
+//将Base的对象c委托给了Derived,Derived可以直接使用对象c的print方法
+class Derived(c:Base):Base by c
+{
+    fun getName():String{
+        return "Sakura"
+    }
+}
+
 open class MyClass
 {
+
     companion object {
         val x = 1
     }
--- day_10/src/main.kt
+++ day_10/src/main.kt
@@ -1,9 +1,21 @@
-import f.MyClass
-import f.process
+import f.*
 
 fun main(args:Array<String>) {
-    val x = MyClass.Companion.x
-    println(x)
+    val c3 = Myclass3()
+    val c4 = Mycalss4()
+    c3.name = "Sakura"
+    c4.name = "Marie"
+
+    val j8:String = "dick"
+    var c1 = MyClass1()
+    var c2 = MyClass2()
+    c1.name = "Yea Sakura"
+    c2.name = ""
+    val b = BaseImpl(10)
+    Derived(b).print()
+
+    val x = MyClass.x
+
     var n = 20
     process(object:MyClass() {
         override fun test(){
